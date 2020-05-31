dd253949673b936dad956f70976e9a77b7c33f71
martinbeck723 <xishuoliu514@gmail.com>
2020-05-31 14:45:01 -0400

    add showing text

--- app/src/main/java/com/example/firstchallenagemeicode/MainActivity.java
+++ app/src/main/java/com/example/firstchallenagemeicode/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.firstchallenagemeicode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onBtnClick(View view){
+     //first get all the text
+        TextView txtFirst= findViewById(R.id.txtFirst);
+        TextView txtLast= findViewById(R.id.txtLast);
+        TextView txtEmail= findViewById(R.id.txtEmail);
+
+        EditText edtTxtLast=findViewById(R.id.edtTxtLast);
+        EditText edtTxtFisrt=findViewById(R.id.edtTxtFirst);
+        EditText edtTxtEmail=findViewById(R.id.edtTxtEmail);
+    //then show it to user
+        txtFirst.setText(edtTxtLast.getText().toString());
+        txtLast.setText(edtTxtFisrt.getText().toString());
+        txtEmail.setText(edtTxtEmail.getText().toString());
     }
 }
